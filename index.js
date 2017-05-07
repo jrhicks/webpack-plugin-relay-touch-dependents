e73b2749b2b832de3397edd76d852a0275102749
Jeffrey <jrhicks@cteh.com>
2017-05-06 21:01:19 -0500

    Update Relay signature.  Add npmignore.  Tweak logging

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var filewatcher = require('filewatcher');
 var touch = require('touch');
+var throttle = require( 'method-throttle' );
 
-var RELAY_SIGNATURE = 'function _taggedTemplateLiteral';
+var RELAY_SIGNATURE = 'Relay.QL';
 
 function WebpackPluginGraphqlJSONHot(options) {
   this.options = options;
   this.log = msg => {
     // if (this.verbose) {
-    console.log('[relay-touch-dependents]:', msg);
+    console.log('[relay-touch-dependents]', msg);
     // }
   };
 
   this.err = msg => {
     // if (!this.hideErrors) {
-    console.error('[ERROR][relay-touch-dependents]:', msg);
+    console.error('[ERROR][relay-touch-dependents]', msg);
     // }
   };
 }
@@ -23,16 +24,26 @@ WebpackPluginGraphqlJSONHot.prototype.apply = function(compiler) {
   var wait = this.options.wait;
   var watcher = filewatcher();
   // var token;
-  var dependents = [];
-
+  var dependents = {};
+  var count = 0;
+  var scanned = 0;
   var that = this;
+  var logUpdate = throttle(function (scanned, count){
+    if (scanned) {
+      that.log("Flagged " + count + "/" + scanned + " modules during compilation as schema dependents." );
+    }
+  }, 5000 );
+  logUpdate();
+  that.log('Version: 0.0.33 (c) MIT Jeffrey Hicks');
   compiler.plugin("compilation", function(compilation) {
     compilation.plugin('succeed-module', function(module){
-      if (!dependents.includes(module.resource)) {
+      if (!dependents[module.resource]) {
+        scanned = scanned + 1;
         if (module.loaders && module.loaders.length > 0 && module._source._value.includes(RELAY_SIGNATURE)) {
-          that.log('Dependent: ' + module.rawRequest);
-          dependents.push(module.resource);
+          dependents[module.resource] = true;
+          count = count + 1;
         }
+        logUpdate(scanned, count);
       }
     });
   });
@@ -43,10 +54,11 @@ WebpackPluginGraphqlJSONHot.prototype.apply = function(compiler) {
     that.log('Schema changed.  Touching dependents in: ' + wait + 'ms');
     setTimeout(
       function() {
-        that.log('Touching ' + dependents.length + ' dependent(s).');
-        dependents.forEach(function(f) {
+        that.log('Touching ' + Object.keys(dependents).length + ' dependent(s).');
+        Object.keys(dependents).forEach(function(f) {
           touch(f, { nocreate: true });
         });
+        // that.log('Done ' + Object.keys(dependents).length + ' dependent(s).');
       }, wait);
     }); // end watcher on
 };
